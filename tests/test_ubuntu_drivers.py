MitchellAugustin/ubuntu-drivers-common

commit b5f42cd4216ec420373ec823f1414c7e4b772d6f

Require root for install

ubuntu-drivers
@@ -412,6 +412,13 @@ def greet(config, gpgpu, free_only, package_list, no_oem, **kwargs):
 @pass_config
 def install(config, **kwargs):
     '''Install a driver [driver[:version][,driver[:version]]]'''
+
+    # Require root
+    if os.geteuid() != 0:
+        print("Error: 'ubuntu-drivers install' must be run as root. Try using 'sudo'.", file=sys.stderr)
+        sys.exit(1)
+
+
     if kwargs.get('gpgpu'):
         config.gpgpu = True
     if kwargs.get('free_only'):

commit caa74696a3570b3a9a009343d0329e7a9ac32850

Allow non-root users using a fake archive to run without sudo (fixes test failures introduced in b5f42cd4216ec420373ec823f1414c7e4b772d6f)

ubuntu-drivers
@@ -460,8 +460,8 @@ def greet(config, gpgpu, free_only, package_list, no_oem, **kwargs):
 def install(config, **kwargs):
     '''Install a driver [driver[:version][,driver[:version]]]'''
 
-    # Require root
-    if os.geteuid() != 0:
+    # Require user to be root or use a fake archive (for tests)
+    if os.geteuid() != 0 and not os.environ["FAKEARCHIVE"]:
         print("Error: 'ubuntu-drivers install' must be run as root. Try using 'sudo'.", file=sys.stderr)
         sys.exit(1)
 
@@ -495,8 +495,8 @@ def autoinstall(config, **kwargs):
     '''Deprecated, please use "install" instead'''
     #print('install, {0}'.format(kwargs['driver']))
 
-    # Require root
-    if os.geteuid() != 0:
+    # Require user to be root or use a fake archive (for tests)
+    if os.geteuid() != 0 and not os.environ["FAKEARCHIVE"]:
         print("Error: 'ubuntu-drivers autoinstall' must be run as root. Try using 'sudo'.", file=sys.stderr)
         sys.exit(1)
 

tests/test_ubuntu_drivers.py
@@ -4901,10 +4901,12 @@ APT::Get::AllowUnauthenticated "true";
     def test_auto_install_chroot(self):
         '''ubuntu-drivers install for fake sysfs and chroot'''
 
+        env = os.environ.copy()
+        env['FAKEARCHIVE'] = 'TRUE'
         ud = subprocess.Popen(
             [self.tool_path, 'install'],
             universal_newlines=True, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE, env=os.environ)
+            stderr=subprocess.PIPE, env=env)
         out, err = ud.communicate()
         # self.assertEqual(err, '')
         self.assertTrue('bcmwl-kernel-source' in out, out)
@@ -4913,10 +4915,12 @@ APT::Get::AllowUnauthenticated "true";
         self.assertEqual(ud.returncode, 0)
 
         # now all packages should be installed, so it should not do anything
+        env = os.environ.copy()
+        env['FAKEARCHIVE'] = 'TRUE'
         ud = subprocess.Popen(
             [self.tool_path, 'install'],
             universal_newlines=True, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE, env=os.environ)
+            stderr=subprocess.PIPE, env=env)
         out, err = ud.communicate()
         # self.assertEqual(err, '')
         self.assertFalse('bcmwl-kernel-source' in out, out)
@@ -4928,10 +4932,12 @@ APT::Get::AllowUnauthenticated "true";
         listfile = os.path.join(self.chroot.path, 'pkgs')
         self.addCleanup(os.unlink, listfile)
 
+        env = os.environ.copy()
+        env['FAKEARCHIVE'] = 'TRUE'
         ud = subprocess.Popen(
             [self.tool_path, 'install', '--package-list', listfile],
             universal_newlines=True, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+            stderr=subprocess.PIPE, env=env)
         out, err = ud.communicate()
         # self.assertEqual(err, '')
         self.assertEqual(ud.returncode, 0)